f318992d512dd3765dfcf568816729c2f344de29
Lucas Gabriel <lucas.gabriel.017@ufrn.edu.br>
2021-07-10 15:58:00 -0300

    teste de disparo 2

--- pag/php/registro.php
+++ pag/php/registro.php
@@ -150,13 +150,33 @@
 
     <?php
       if($_GET['msg']){ 
-        echo " 
-        <div>
-          <h1> Seja bem vindo a nossa plataforma</h1>
-        </div>
-        ";
+        <script>
+          $('#modalsucesso').modal('show');
+        </script>
       }
     ?>
+
+    <!-- Modal -->
+    <div class="modal fade" id="modalsucesso" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            ...
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </body>	
 
 </html>

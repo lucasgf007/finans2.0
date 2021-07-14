0ca5327600bdd0d498aba5725e98b3a3c382f672
Lucas Gabriel <lucas.gabriel.017@ufrn.edu.br>
2021-07-14 19:28:13 -0300

    validar user

---
+++ pag/php/validar_user.php
@@ -0,0 +1,28 @@
+<?php include("conexao.php")?>
+
+<?php
+    $login = $_POST["login"];
+    $senha = $_POST["senha"];
+    $estaLogado = false;
+
+
+    $sql = "SELECT * FROM despesas";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // comparar
+    while($row = $result->fetch_assoc()) {
+        if(($row["email"] == $login || $row["nome"] == $login) && $row["senha"] == sha1($senha)){
+            $estaLogado = true;
+            header("Location: registro.php?msg=sucesso");
+
+                    //echo "  /Ano: " . $row["ano"]. "  " . "/Mes: " . $row["mes"]. " " . "/Dia: ". $row["dia"]. " " . "/Tipo: ". $row["tipo"]. " " . "/Descrição: ". $row["descricao"]. " " . "/Valor: ". $row["valor"]."<br>";
+        } else{
+            echo "error";
+        }
+    }
+    } else {
+        echo "0 results";
+    }
+    $conn->close();
+?>

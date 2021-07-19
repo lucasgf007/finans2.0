21217f990a5434cfc054489e35b7fa0a2f0af56d
Lucas Gabriel <lucas.gabriel.017@ufrn.edu.br>
2021-07-18 21:21:52 -0300

    teste final

--- pag/php/registrar_noBanco/registrar_despesa.php
+++ pag/php/registrar_noBanco/registrar_despesa.php
@@ -8,17 +8,17 @@
         $descricao =  $_POST["descricao"];
         $valor =  $_POST["valor"];
 
-        echo "  /Ano: " . $_POST["ano"]. "  " . "/Mes: " . $_POST["mes"]. " " . "/Dia: ". $_POST["dia"]. " " . "/Tipo: ". $_POST["tipo"]. " " . "/Descrição: ". $_POST["descricao"]. " " . "/Valor: ". $_POST["valor"]."<br>";
+        //echo "  /Ano: " . $_POST["ano"]. "  " . "/Mes: " . $_POST["mes"]. " " . "/Dia: ". $_POST["dia"]. " " . "/Tipo: ". $_POST["tipo"]. " " . "/Descrição: ". $_POST["descricao"]. " " . "/Valor: ". $_POST["valor"]."<br>";
     
         
-        // $sql = "INSERT INTO despesas (ano, mes, dia, tipo, descricao, valor) 
-        // VALUES ( '".$ano."', '".$mes."', '".$dia."', '".$tipo."', '".$descricao."', '".$valor."')";
+        $sql = "INSERT INTO despesas (ano, mes, dia, tipo, descricao, valor) 
+        VALUES ( '".$ano."', '".$mes."', '".$dia."', '".$tipo."', '".$descricao."', '".$valor."')";
         
-        // if ($conn->query($sql) === TRUE) {
-        //   header("Location: ../registro.php?msg=cadastrou");
-        // } else {
-        //   echo "Error: " . $sql . "<br>" . $conn->error;
-        // }
+        if ($conn->query($sql) === TRUE) {
+          header("Location: ../registro.php?msg=cadastrou");
+        } else {
+          echo "Error: " . $sql . "<br>" . $conn->error;
+        }
         
-        // $conn->close();
+        $conn->close();
 ?>

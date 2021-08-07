d8528d8677da6f5313fddda64798aee6c82b8f8c
Lucas Gabriel <lucas.gabriel.017@ufrn.edu.br>
2021-08-07 16:26:30 -0300

    teste

--- pag/php/consulta/remove.php
+++ pag/php/consulta/remove.php
@@ -8,14 +8,14 @@ session_start();
 
 $id = $_GET['id'];  
     // sql to delete a record
-$sql = "DELETE FROM despesas WHERE id = '$id'";
+$sql = "DELETE FROM despesas WHERE id = '7'";
 
-// if ($conn->query($sql) === TRUE) {
-//   header('Location: ../consulta.php?msg=removeSucess');
+if ($conn->query($sql) === TRUE) {
+  header('Location: ../consulta.php?msg=removeSucess');
   
-// } else {
-//   echo "Error deleting record: " . $conn->error;
-// }
+} else {
+  echo "Error deleting record: " . $conn->error;
+}
 
 $conn->close();
 ?>

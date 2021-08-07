60dfed0fd441a9cee612dc2c1cd6b9d13367d493
Lucas Gabriel <lucas.gabriel.017@ufrn.edu.br>
2021-08-07 16:30:22 -0300

    teste 3

--- pag/php/consulta/remove.php
+++ pag/php/consulta/remove.php
@@ -6,9 +6,23 @@ session_start();
 
 <?php
 
+    $host = "localhost";
+    $usuario = "u158397775_meu_banco";
+    $senha = "1A2b3c4d@";
+    $bd = "u158397775_banco_finans";
+    
+    // Create connection
+    $conn = new mysqli($host, $usuario, $senha, $bd); 
+
+    // Check connection
+    if ($conn->connect_error) {
+        
+        echo "Falha na conexão: (".$conn->connect_error.") ".$conn->connect_error;
+    }
+
 $id = $_GET['id'];  
     // sql to delete a record
-$sql = "DELETE FROM despesas WHERE id = 7";
+$sql = "DELETE FROM despesas WHERE id=7";
 
 if ($conn->query($sql) === TRUE) {
   header('Location: ../consulta.php?msg=removeSucess');

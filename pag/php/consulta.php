6fa2ca607d51e4be86f02a1fc8f90a8ca6f7f3d4
Lucas Gabriel <lucas.gabriel.017@ufrn.edu.br>
2021-08-17 20:03:20 -0300

    consulta

--- pag/php/consulta.php
+++ pag/php/consulta.php
@@ -188,9 +188,13 @@ session_start();
               
      if ($result->num_rows > 0) {
                       // output data of each row
-        while($row = $result->fetch_assoc()) {
-          echo " /Ano: " . $row["ano"]. "  " . "/Mes: " . $row["mes"]. " " . "/Dia: ". $row["dia"]. " " . "/Tipo: ". $row["tipo"]. " " . "/Descrição: ". $row["descricao"]. " " . "/Valor: ". $row["valor"]. "  " . "<a href='consulta/remove.php?id=". $row["id"]."'> Excluir </a> <br>";
-        }
+        
+          while($row = $result->fetch_assoc()) {
+            if($row["id_usuario"] == $_SESSION["ID"]){
+              echo " /Ano: " . $row["ano"]. "  " . "/Mes: " . $row["mes"]. " " . "/Dia: ". $row["dia"]. " " . "/Tipo: ". $row["tipo"]. " " . "/Descrição: ". $row["descricao"]. " " . "/Valor: ". $row["valor"]. "  " . "<a href='consulta/remove.php?id=". $row["id"]."'> Excluir </a> <br>";
+            }
+          }
+        
      } else {
       echo "0 results";
       }
